b400d1fd304bced67d299eced22059ed0d582a2b
redpotatous <43158933+tomgros@users.noreply.github.com>
2020-01-22 13:12:24 -0800

    Organize the 'while' loop a bit differently so multiple calls to same code can be eliminated. Change how the cumulative cost of the shortest path is calculated (previous calculation was getting 'false' shortest paths).

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ struct traverse_info {
 
 int main() {
 
-    string input, cur_node, end_node;
+    string cur_node, end_node;
 
     // tbg Graph dg { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p" };
     Graph dg { "a", "b", "c", "d", "e" };
@@ -31,6 +31,7 @@ int main() {
     dg.create_density(0.5);
 
     // Print 'dg'.
+    //
     cout << "With density of 50%: (dg)" << endl;
     cout << dg << endl;
 
@@ -40,88 +41,69 @@ int main() {
     unordered_map <string, traverse_info> closed;
     vector <traverse_info>                open;
 
-    // Ask the user to enter a start node.
+    // Ask the user to enter a start node.  Make sure the
+    // user entered a node that's in the graph.
+    //
     cout << "Please enter a starting node: "s;
-    cin >> cur_node;
-
-    // Make sure the user entered a cur_node that's in the graph.
+    cin  >> cur_node;
     if (!dg.contains(cur_node)) {
         cout << "The start node entered is not in the graph: "s << cur_node << endl;
         return 1;
     }
 
-    // Ask the user to enter an end node.
+    // Ask the user to enter an end node.  Make sure the
+    // user entered a node that's in the graph.
+    //
     cout << "Please enter an end node: "s;
-    cin >> end_node;
-
-    // Make sure the user entered a end_node that's in the graph.
+    cin  >> end_node;
     if (!dg.contains(end_node)) {
         cout << "The end node entered is not in the graph: "s << end_node << endl;
         return 1;
     }
 
-    // Add the cur_node to the 'closed' list.
+    // Add the cur_node (start node) to the 'closed' list.
     //
     closed.insert( {cur_node, {cur_node, cur_node, 0}} );
 
-    // Get the nodes/costs reachable from the cur_node.
-    unique_ptr< vector<reachable_node> > rn = dg.get_reachable_nodes (cur_node);
-
-    // Add nodes reachable from the cur_node to the 'open list'
-    // if they are not already in the 'closed list'.
+    // Loop through the nodes in this Graph to determine if there's
+    // a path from the start node to the 'end_node'.
     //
-    for (auto& [name, cost] : *rn) {
-            cout << "name: " << name << ", cost: " << cost << endl; // tbg
-        if (!closed.contains(name)) {
-            cout << "    put on open list: name: " << name << ", cost: " << cost << endl; // tbg
-            open.push_back( {cur_node, name, cost} );
-        }
-    }
+    while (!closed.contains(end_node)) {
 
-    cout << endl; // tbg
-
-    // Use a lambda expression to sort the 'open' list in desending order
-    //
-    sort (open.begin(), open.end(), [](traverse_info& a, traverse_info& b) {
-        return a.total_cost > b.total_cost;
-    });
-
-    while (!closed.contains(end_node) && !open.empty()) {
-
-        // Get the next node that has the smallest total cost.
-        traverse_info next_node = open.back();
-        open.pop_back();
-
-        // Add this 'next' node to the closed list. Determine what
-        // nodes are reachable from this 'next' node and add them to
-        // the open list.  Continue checking to see if the end_node
-        // has been added to the closed list.
+        // Get the nodes/costs reachable from the cur_node.
         //
-        closed.insert( {next_node.node_id, {next_node.from_node,
-                                            next_node.node_id,
-                                            next_node.total_cost + closed.at(next_node.from_node).total_cost}} );
+        unique_ptr< vector<reachable_node> > rn = dg.get_reachable_nodes (cur_node);
 
-        // Get the nodes/costs reachable from this next_node.
-        unique_ptr< vector<reachable_node> > rn = dg.get_reachable_nodes (next_node.node_id);
-
-        // Add nodes reachable from this next_node to the 'open list'
-        // if they are not already in the 'closed list'.
+        // Add nodes reachable from the cur_node to the 'open list' if
+        // they are not already in the 'closed list'.
         //
         for (auto& [name, cost] : *rn) {
-                cout << "name: " << name << ", cost: " << cost << endl; // tbg
             if (!closed.contains(name)) {
-                cout << "    put on open list: name: " << name << ", cost: " << cost << endl; // tbg
-                open.push_back( {next_node.node_id, name, cost} );
+                open.push_back( {cur_node, name, cost + closed.at(cur_node).total_cost} );
             }
         }
 
-    cout << endl; // tbg
+        // If there are no entries in the open list break out of the loop.
+        //
+        if (open.empty()) break;
 
         // Use a lambda expression to sort the 'open' list in desending order
         //
         sort (open.begin(), open.end(), [](traverse_info& a, traverse_info& b) {
             return a.total_cost > b.total_cost;
         });
+
+        // Get the next node that has the smallest cost (the last entry
+        // on the open list). Remove it from the open list and set
+        // cur_node to this 'next' node.
+        //
+        traverse_info next_node = open.back();
+        open.pop_back();
+        cur_node = next_node.node_id,
+
+        // Add the 'current' node to the closed list.
+        //
+        closed.insert( {cur_node, {next_node.from_node, cur_node, next_node.total_cost}} );
     }
 
     if (closed.contains(end_node)) {
@@ -129,12 +111,13 @@ int main() {
         string node = end_node;
         cout << closed.at(node).node_id << " - "s << closed.at(node).total_cost << endl;
         while (closed.at(node).node_id != closed.at(node).from_node) {
-            cout << closed.at(node).from_node << " - "s << closed.at(node).total_cost << endl;
+            // tbg cout << closed.at(node).from_node << " - "s << closed.at(node).total_cost << endl;
+            string from_node = closed.at(node).from_node;
+            cout << from_node << " - "s << closed.at(from_node).total_cost << endl;
             node = closed.at(node).from_node;
         }
     }
     else {
         cout << "No path found"s << endl;
     }
-
 }
